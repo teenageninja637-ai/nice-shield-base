fd0b55e27101e3d73e1394eef8b6ee8e77f0d392
Chance <teenageninja637@gmail.com>
2026-04-06 03:03:03 -0500

    un-commented more anim functions in s_p

--- boards/shields/nice_shield_base/widgets/screen_peripheral.c
+++ boards/shields/nice_shield_base/widgets/screen_peripheral.c
@@ -112,8 +112,8 @@ int zmk_widget_screen_init(struct zmk_widget_screen *widget, lv_obj_t *parent) {
     lv_obj_align(top, LV_ALIGN_TOP_RIGHT, 0, 0);
     lv_canvas_set_buffer(top, widget->cbuf, BUFFER_SIZE, BUFFER_SIZE, LV_IMG_CF_TRUE_COLOR);
 
-    draw_right_image(widget->obj);
-    // draw_right_animation(widget->obj);
+    //draw_right_image(widget->obj);
+    draw_right_animation(widget->obj);
   
     sys_slist_append(&widgets, &widget->node);
     widget_battery_status_init();
